5b6fbbc6bd8e8651050689632e29e98d24e51127
Spinning Plates <alec.munguia@gmail.com>
2019-04-11 18:11:38 -0500

    cleaned code and added comments

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,18 +1,20 @@
 $(document).ready(function() {
-        // TV shows
+        // TV shows array
     var topics = ["Breaking Bad", "Bob's Burgers", "Intervention", "South Park", "Archer"];
     var tvShow = "Breaking Bad"; //for testing
     
     function displayGifs() {
-        var gif = $(this).attr("data-name");
-        var queryURL = "https://api.giphy.com/v1/gifs/random?&api_key=rzO0fPt9a1vAJenHpLHsuvuxRwJjB4sg&tag=" + topics[i];
+      var show = $(this).attr("data-name");
+        //THIS DOESN'T WORK AND IT'S PISSING ME OFF
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=rzO0fPt9a1vAJenHpLHsuvuxRwJjB4sg";
 
         $.ajax({
             url: queryURL,
             method: "GET"
             }).then(function(response) {
               var imageDiv = $("<div class = 'show'>");
-              var rating = response.rating;
+              var rating = response.data.rating;
+              console.log(rating);
               var displayRating = $("<p>").text("Rating: " + rating);
               imageDiv.append(displayRating);
               var imageUrl = response.data.image_original_url;
@@ -27,36 +29,34 @@ $(document).ready(function() {
             }
         }
 
-
-
+// make buttons from our topics array
 function renderButtons() {    
-$('#buttons').empty();
-for (var i = 0; i < topics.length; i++) {
-    var a = $("<button>");
-    a.addClass("show-button");
-    a.attr("data-name", topics[i]);
-    a.text(topics[i]);
-    $('#buttons').append(a);
+  $('#buttons').empty();
+    for (var i = 0; i < topics.length; i++) {
+        var a = $("<button>");
+        a.addClass("show-button");
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
+        $('#buttons').append(a);
+      }
+  }
 
-   // $("#buttons").append("<button>" + topics[i] + "</button>");
-}
-}
-
-$(document).on("click", ".show-button", displayGifs);
-
+// call function to add the buttons
 renderButtons();
 
+// make the buttons run the displayGifs function when clicked
+$(document).on("click", ".show-button", displayGifs);
 
-
+// form to add show
 $("#add-show").on("click", function(event) {
   event.preventDefault();
-  // This line of code will grab the input from the textbox
+  // Grab the input from the textbox
   var show = $("#show-input").val().trim();
 
-  // The movie from the textbox is then added to our array
+  // added to topics array
   topics.push(show);
 
-  // Calling renderButtons which handles the processing of our movie array
+  // Make a button for it
   renderButtons();
 
 });
