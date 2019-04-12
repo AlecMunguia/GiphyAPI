e3a10fb5171cab42b48e1c5c5e380c4be1de610d
Spinning Plates <alec.munguia@gmail.com>
2019-04-12 12:14:16 -0500

    commit before tutoring

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,31 +1,28 @@
 $(document).ready(function() {
         // TV shows array
     var topics = ["Breaking Bad", "Bob's Burgers", "Intervention", "South Park", "Archer"];
-    var tvShow = "Breaking Bad"; //for testing
     
     function displayGifs() {
       var show = $(this).attr("data-name");
-        //THIS DOESN'T WORK AND IT'S PISSING ME OFF
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=rzO0fPt9a1vAJenHpLHsuvuxRwJjB4sg";
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=rzO0fPt9a1vAJenHpLHsuvuxRwJjB4sg&limit=10";
 
         $.ajax({
             url: queryURL,
             method: "GET"
             }).then(function(response) {
               var imageDiv = $("<div class = 'show'>");
-              var rating = response.data.rating;
-              console.log(rating);
-              var displayRating = $("<p>").text("Rating: " + rating);
-              imageDiv.append(displayRating);
-              var imageUrl = response.data.image_original_url;
+              var imageUrl = response.data[i].images.fixed_height.url;
               var showImage = $("<img>");
               showImage.attr("src", imageUrl);
-              showImage.attr("alt", "show image");
               $("#images").prepend(showImage);
+
+              var rating = response.data.rating;
+              var displayRating = $("<p>").text("Rating: " + rating);
+              imageDiv.append(displayRating);
             });   
 
             for (var i=0; i<topics.length; i++) {
-              console.log(topics[i]);
+              
             }
         }
 
@@ -61,20 +58,8 @@ $("#add-show").on("click", function(event) {
 
 });
 
-
-
-
-
-
-
-
-
-
-
-// have javascript populate the buttons (10 displayed at load)
-// on click load 10 images from the Giphy API
-// gifs play and pause on click
-// input form to add new buttons to the page with same functionality
-
-
-})
+                            // NOTES FOR TUTORING:
+                            // the ajax request is returning the 10 images but only one is appending
+                            // the rating is not loading 
+                            // they need to load paused and play on click (data state - data-still - data-animate)
+                            })
