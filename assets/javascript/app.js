326716234418fbf9c0104bc2a63c2efd05c1b71a
Spinning Plates <alec.munguia@gmail.com>
2019-04-12 19:34:24 -0500

    finished

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,30 +1,53 @@
 $(document).ready(function() {
         // TV shows array
-    var topics = ["Breaking Bad", "Bob's Burgers", "Intervention", "South Park", "Archer"];
+    var topics = ["Louie", "Bob's Burgers", "Peep Show", "South Park", "Archer"];
     
     function displayGifs() {
       var show = $(this).attr("data-name");
+      $("#images").empty();
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=rzO0fPt9a1vAJenHpLHsuvuxRwJjB4sg&limit=10";
 
         $.ajax({
             url: queryURL,
             method: "GET"
             }).then(function(response) {
-              var imageDiv = $("<div class = 'show'>");
-              var imageUrl = response.data[i].images.fixed_height.url;
-              var showImage = $("<img>");
-              showImage.attr("src", imageUrl);
-              $("#images").prepend(showImage);
-
-              var rating = response.data.rating;
-              var displayRating = $("<p>").text("Rating: " + rating);
-              imageDiv.append(displayRating);
-            });   
-
-            for (var i=0; i<topics.length; i++) {
               
-            }
+              for(var i = 0; i < response.data.length; i++){
+                var imageDiv = $("<div>");
+
+                var stillImageUrl = response.data[i].images.fixed_height_still.url;
+                var animatedUrl = response.data[i].images.fixed_height.url;
+                var showImage = $("<img>");
+                showImage.addClass("show");
+                showImage.attr("src", stillImageUrl);
+                showImage.attr("data-stillUrl", stillImageUrl);
+                showImage.attr("data-animateUrl", animatedUrl);
+
+                imageDiv.append(showImage);
+
+                var rating = response.data[i].rating;
+                var displayRating = $("<p>").text("Rating: " + rating);
+                imageDiv.append(displayRating);
+
+                $("#images").append(imageDiv);
+              }
+
+            });  
         }
+
+$(document).on("click", ".show", function() {
+  var src = $(this).attr("src");
+  var stillUrl = $(this).attr("data-stillUrl");
+  var animateUrl = $(this).attr("data-animateUrl");
+  console.log(`src: ${src}`);
+  console.log(`data attr: ${stillUrl}`)
+
+  if(src === stillUrl){
+    $(this).attr("src", animateUrl);
+  } else {
+    $(this).attr("src", stillUrl);
+  }
+})
 
 // make buttons from our topics array
 function renderButtons() {    
